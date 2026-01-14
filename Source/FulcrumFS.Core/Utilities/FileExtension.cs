23f2e9ef83913b9a5ff056816e1e85b69a2ed2b9
Hamish Arblaster <hamarb123@gmail.com>
2026-01-14 18:32:47 +1100

    Add missing vectorized-enough check for "is lower-case"

--- Source/FulcrumFS.Core/Utilities/FileExtension.cs
+++ Source/FulcrumFS.Core/Utilities/FileExtension.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Text;
 
 namespace FulcrumFS.Utilities;
 
@@ -32,5 +33,19 @@ public static class FileExtension
     /// <summary>
     /// Checks if the given file extension is valid and normalized.
     /// </summary>
-    public static bool IsValidAndNormalized(ReadOnlySpan<char> extension) => PathFormat.Universal.IsValidExtension(extension);
+    public static bool IsValidAndNormalized(ReadOnlySpan<char> extension)
+    {
+        if (!PathFormat.Universal.IsValidExtension(extension)) return false;
+        if (extension.ContainsAnyInRange('A', 'Z')) return false;
+
+        int nonAsciiIndex = extension.IndexOfAnyInRange((char)0x0080, (char)0xFFFF);
+        while (nonAsciiIndex >= 0)
+        {
+            if (char.IsUpper(extension[nonAsciiIndex])) return false;
+            extension = extension[(nonAsciiIndex + 1)..];
+            nonAsciiIndex = extension.IndexOfAnyInRange((char)0x0080, (char)0xFFFF);
+        }
+
+        return true;
+    }
 }
